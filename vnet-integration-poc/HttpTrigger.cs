6e6c83457c0d9bb7696196eb89be8c8d04737024
aboondooley-seagen <aboondooley@seagen.com>
2023-03-17 16:56:34 -0700

    Taking out complex part of function

--- vnet-integration-poc/HttpTrigger.cs
+++ vnet-integration-poc/HttpTrigger.cs
@@ -19,7 +19,7 @@ namespace vnet_integration_poc
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            /*string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
@@ -27,9 +27,9 @@ namespace vnet_integration_poc
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";*/
 
-            return new OkObjectResult(responseMessage);
+            return new OkResult();
         }
     }
 }
