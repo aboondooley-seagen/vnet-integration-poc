b3bfffd3188e1bab5e2216d42e038d56ba3344c2
aboondooley-seagen <aboondooley@seagen.com>
2023-03-21 22:29:00 -0700

    Added call to database to test connection.

--- vnet-integration-poc/ServiceBusQueueTrigger.cs
+++ vnet-integration-poc/ServiceBusQueueTrigger.cs
@@ -1,13 +1,46 @@
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 
 namespace vnet_integration_poc
 {
     public class ServiceBusQueueTrigger
     {
         [FunctionName("ServiceBusQueueTrigger")]
-        public void Run([ServiceBusTrigger("queue", Connection = "SERVICEBUS_CONNECTION")]string myQueueItem, ILogger log)
+        public async Task Run([ServiceBusTrigger("queue", Connection = "SERVICEBUS_CONNECTION")]string myQueueItem, ILogger log)
         {
+            log.LogInformation($"C# ServiceBus queue trigger function beginning to process message: {myQueueItem}");
+
+            string databaseName = "dbTest";
+            string collectionName = "collectionTest";
+            dynamic data = new System.Dynamic.ExpandoObject();
+            data.id = Guid.NewGuid().ToString();
+            data.databaseName = databaseName;
+            data.collectionName = collectionName;
+            data.name = myQueueItem;
+
+            var connectionString = Environment.GetEnvironmentVariable("COSMOSDB_CONNECTION");
+
+            try
+            {
+                using (CosmosClient client = new CosmosClient(connectionString))
+                {
+                    log.LogInformation($"C# ServiceBus queue trigger function connected to cosmos db");
+                    var database = client.GetDatabase(databaseName);
+                    var container = database.GetContainer(collectionName);
+
+                    var response = await container.CreateItemAsync(data);
+                    log.LogInformation($"C# ServiceBus queue trigger function added data to database: {data}");
+                }
+            }
+            catch (Exception ex)
+            {
+                data.errorMessage = ex.Message;
+            }
+
+
             log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
         }
     }
